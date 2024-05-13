aa357edf275872762fc6b33475c9bc8bad471677
Harsh Pandhe <harshpandhehome@gmail.com>
2024-05-13 22:18:51 +0530

    Did Control Statements and Added a new file Loops and Covered all the topics there.

---
+++ loops.py
@@ -0,0 +1,51 @@
+# Loops
+
+# for loop
+for i in range(5):
+    print(i)
+
+for i in range(1, 5):
+    print(i)
+
+
+# Tables
+
+for i in range(1, 11):
+    print(f"5 x {i} = {5 * i}")
+
+# Pattern
+
+rows = 5 # int(input("Enter the number of rows: "))
+for i in range(rows):
+    for j in range(i + 1):
+        print("*", end="")
+    print()
+
+# while loop
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+
+# break statement
+
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
+
+# continue statement
+
+for i in range(10):
+    if i == 5:
+        continue
+    print(i)
+
+# pass statement
+
+for i in range(10):
+    if i == 6:
+        print("Before Pass")
+        pass
+        print("After Pass")
+    print(i)
