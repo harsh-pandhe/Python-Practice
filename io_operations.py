24f2bc32089fbba0d18bf7cad314660ec13d04b8
Harsh Pandhe <harshpandhehome@gmail.com>
2024-05-15 01:35:37 +0530

    I/O Operations

---
+++ io_operations.py
@@ -0,0 +1,64 @@
+# str = input("Enter a string: ")
+# print("The string is: ", str)
+
+f = open("test.txt", "r")
+if f:
+    print("File opened successfully")
+    print("The file content is: ")
+    print(f.read())
+    f.close()
+
+f = open("test.txt", "r")
+if f:
+    print("File opened successfully")
+    print("The file content is: ")
+    print(f.read(4))
+    f.close()
+
+f = open("test.txt", "r")
+if f:
+    print("File opened successfully")
+    print("The file content is: ")
+    print(f.readline())
+    print(f.readline())
+    f.close()
+
+f = open("test.txt", "r")
+if f:
+    print("File opened successfully")
+    print("The file content is: ")
+    for a in f:
+        print(a, end="")
+
+f = open("test.txt", "r")
+if f:
+    print("File opened successfully")
+    print("The file content is: ")
+    print(f.readlines())
+    f.close()
+
+f = open("text.txt", "a")
+if f:
+    print("File opened successfully")
+    f.write("This is the appended line")
+    f = open("test.txt", "r")
+    print(f.read())
+    f.close()
+
+import os
+
+# os.rename("test.txt", "test1.txt")
+
+try:
+    print("Tried")
+except:
+    print("Exception")
+
+try:
+    print(k)
+except NameError:
+    print("NameError")
+except:
+    print("Exception")
+
+
